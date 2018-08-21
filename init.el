b9c392451b5aa76f43b2d01996da09fa313b5e62
liushihao <liushihao@pku.edu.cn>
2018-08-21 14:12:15 +0800

    Modified a lot

--- init.el
+++ init.el
@@ -17,16 +17,17 @@
   (package-refresh-contents))
 (eval-when-compile
   (add-to-list 'load-path "~/.emacs.d/packages/use-package")
+  (add-to-list 'load-path "~/.emacs.d/packages/bind-key")
   (require 'use-package))
 
 (setq use-package-compute-statistics t)
 
-(use-package auto-package-update
-  :ensure t
-  :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
+;; (use-package auto-package-update
+;;   :ensure t
+;;   :config
+;;   (setq auto-package-update-delete-old-versions t)
+;;   (setq auto-package-update-hide-results t)
+;;   (auto-package-update-maybe))
 
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
@@ -44,18 +45,9 @@
  '(global-hl-line-mode t)
  '(package-selected-packages
    (quote
-    (company-rtags helm-rtags rtags treemacs-projectile treemacs anaconda company-anaconda jedi py-autopep8 ein matlab-mode matlab helm-swoop dashboard auctex-latexmk cdlatex helm-bibtex auctex company-lsp lsp-java lsp-ui lsp-mode company-irony irony company-jedi jedi-core dumb-jump helm-projectile projectile smartparens hydra aggressive-indent auto-yasnippet multiple-cursors expand-region hungry-delete undo-tree company yasnippet-snippets yasnippet ace-window which-key powerline zerodark-theme use-package auto-package-update)))
+    (helm-ag dashboard matlab-mode auctex-latexmk cdlatex helm-bibtex auctex company-lsp lsp-java lsp-ui lsp-mode company-irony irony ein py-autopep8 jedi treemacs-projectile treemacs dumb-jump helm-swoop helm-projectile projectile smartparens hydra aggressive-indent auto-yasnippet multiple-cursors expand-region hungry-delete undo-tree company yasnippet-snippets yasnippet ace-window which-key powerline zerodark-theme auto-package-update)))
  '(python-shell-interpreter "python3")
  '(scroll-bar-mode nil)
- '(treemacs-collapse-dirs 3 t nil "Customized with use-package treemacs")
- '(treemacs-filewatch-mode t)
- '(treemacs-follow-after-init t t nil "Customized with use-package treemacs")
- '(treemacs-follow-mode t)
- '(treemacs-fringe-indicator-mode nil)
- '(treemacs-git-mode (quote deferred))
- '(treemacs-goto-tag-strategy (quote refetch-index) t nil "Customized with use-package treemacs")
- '(treemacs-indentation 2 t nil "Customized with use-package treemacs")
- '(treemacs-is-never-other-window nil t nil "Customized with use-package treemacs")
  '(truncate-lines t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -80,17 +72,23 @@
       mac-option-modifier 'none)
 (set-frame-font "Monaco-12") ; set font to Monaco
 
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'zenburn-theme t)
+;;   )
+
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;;   (load-theme 'doom-one t)
+;;   )
+
 (use-package zerodark-theme
   :ensure t
   :config
   (load-theme 'zerodark t)
   )
-
-;; (use-package dracula-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'dracula t)
-;;   )
 
 (use-package powerline
   :ensure t
@@ -239,8 +237,7 @@ narrowed."
 						    text-mode yaml-mode java-mode))
   :hook ((emacs-lisp-mode . aggressive-indent-mode)
 	 (c-mode . aggressive-indent-mode)
-	 (c++-mode . aggressive-indent-mode)
-	 (LaTeX-mode . aggressive-indent-mode))
+	 (c++-mode . aggressive-indent-mode))
   :config
   (setq-local electric-indent-mode nil)
   (defun turn-off-electric-indent-mode ()
@@ -370,6 +367,14 @@ _d_: dir"
 
 (use-package dumb-jump
   :ensure t
+  :init
+  (defun dumb-jump-go/helm (p)
+    "Load helm before calling treemancs-projectile."
+    (interactive "P")
+    (unless (featurep 'helm)
+      (require 'helm))
+    (dumb-jump-go)
+    )
   :custom
   (dumb-jump-force-searcher 'ag)
   (dumb-jump-selector 'helm)
@@ -378,7 +383,7 @@ _d_: dir"
 			  "
                                Dumb Jump
 ^^^^^^^^----------------------------------------------------------------------"
-			  ("j" dumb-jump-go "Go")
+			  ("j" dumb-jump-go/helm "Go")
 			  ("o" dumb-jump-go-other-window "Other window")
 			  ("e" dumb-jump-go-prefer-external "Go external")
 			  ("b" dumb-jump-back "Back")
@@ -413,7 +418,6 @@ _d_: dir"
   :config
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
-  (treemacs-resize-icons 44)
   :bind (("C-x a" . hydra-treemacs/body)
 	 ("C-c a" . treemacs-helpful-hydra))
   :hydra (hydra-treemacs (:color blue :hint nil)
@@ -424,7 +428,8 @@ _d_: dir"
 ^ ^
 _a_: Treemacs                       _s_: Select treemacs window        _f_: Find files
 ^ ^
-_p_: Treemacs projectile            _d_: Delete other windows          _b_: Find bookmark"
+_p_: Treemacs projectile            _d_: Delete other windows          _b_: Find bookmark
+^ ^"
                          ("a" treemacs)
 			 ("s" treemacs-select-window)
 			 ("f" treemacs-find-file)
@@ -599,7 +604,8 @@ _p_: Treemacs projectile            _d_: Delete other windows          _b_: Find
   :config
   (setq TeX-auto-save t)
   (reftex-mode t)
-  (setq TeX-engine 'xetex)
+  (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+  (add-hook 'LaTeX-mode-hook (lambda () (setq TeX-engine 'xetex)))
   (setq TeX-view-program-selection
 	'((output-dvi "open")
 	  (output-pdf "Preview.app")
@@ -630,282 +636,7 @@ _p_: Treemacs projectile            _d_: Delete other windows          _b_: Find
 (use-package matlab-mode
   :ensure t
   :commands matlab-shell
-  :defer t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                Smartparens                                ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defhydra hydra-smartparens (:hint nil)
-;;   "
-;;                                Smartparens
-;; ^^^^^^^^------------------------------------------------------------------------------
-;; _d_: down                         _a_: Down                     _f_: forward
-;; _e_: up                           _u_: Up                       _b_: backward
-;; _k_: kill                         _q_: quit
-;; "
-;;   ("d" sp-down-sexp)
-;;   ("e" sp-up-sexp)
-;;   ("u" sp-backward-up-sexp)
-;;   ("a" sp-backward-down-sexp)
-;;   ("f" sp-forward-sexp)
-;;   ("b" sp-backward-sexp)
-;;   ("k" sp-kill-sexp)
-;;   ("q" nil :color blue))
-;; (define-key smartparens-mode-map (kbd "C-c s") 'hydra-smartparens/body)
-;; (define-key smartparens-mode-map (kbd "C-k") 'sp-kill-hybrid-sexp)
-;; (define-key smartparens-mode-map (kbd "M-k") 'sp-backward-kill-sexp)
-;; (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
-;; (define-key smartparens-mode-map (kbd "M-<right>") 'sp-forward-barf-sexp)
-;; (define-key smartparens-mode-map (kbd "C-<left>") 'sp-backward-slurp-sexp)
-;; (define-key smartparens-mode-map (kbd "M-<left>") 'sp-backward-barf-sexp)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                 Projectile                                ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (helm-projectile-on)
-
-;; (define-key projectile-mode-map (kbd "C-c C-p") nil)
-;; (defhydra hydra-helm-projectile (:color blue
-;; 					:hint nil)
-;;   "
-;; ^Find^                            ^List^                          ^Grep^
-;; ^^^^^^^^-------------------------------------------------------------------------------
-;; _f_: files                        _e_: recent files               _s s_: ag
-;; _a_: other files                  _p_: proj                       _s g_: grep
-;; _F_: files all prj                _b_: buff curr proj
-;; _g_: files at point               _h_: helm interface
-;; _d_: dir"
-;;   ("f" helm-projectile-find-file)
-;;   ("a" helm-projectile-find-other-file)
-;;   ("F" helm-projectile-find-file-in-known-projects)
-;;   ("g" helm-projectile-find-file-dwim)
-;;   ("d" helm-projectile-find-dir)
-
-;;   ("e" helm-projectile-recentf)
-;;   ("p" helm-projectile-switch-project)
-;;   ("b" helm-projectile-switch-to-buffer)
-;;   ("h" helm-projectile)
-
-;;   ("s s" helm-projectile-ag)
-;;   ("s g" helm-projectile-grep))
-;; (define-key projectile-mode-map (kbd "C-c p") 'hydra-helm-projectile/body)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                    Helm                                   ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (require 'helm-config)
-;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-;; (global-set-key (kbd "C-x b") 'helm-mini)
-;; (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
-;; (global-set-key (kbd "C-s") 'helm-swoop)
-;; (global-set-key (kbd "C-r") 'helm-swoop-back-to-last-point)
-;; ;; (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
-;; ;; (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
-
-;; (defhydra hydra-helm (:color blue :columns 3)
-;;   "
-;;                        C-x c Helm Commands
-;; ^^^^^^^^------------------------------------------------------------------"
-;;   ("/" Helm-find "find")
-;;   ("b" helm-resume "resume")
-;;   ("c" helm-colors "colors")
-
-;;   ("i" helm-semantic-or-imenu "imenu")
-;;   ("a" helm-apropos "apropos")
-;;   ("8" helm-ucs "ucs")
-
-;;   ("I" helm-imenu-in-all-buffers "imenu all buffers")
-;;   ("m" helm-man-woman "man")
-;;   ("p" helm-list-emacs-process "list emacs process")
-
-;;   ("s" helm-do-grep-ag "ag")
-;;   ("t" helm-bibtex "bibtex"))
-;; (global-set-key (kbd "C-x c") 'hydra-helm/body)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                 Dumb jump                                 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (defhydra hydra-dumb-jump (:color blue :columns 3)
-;;   "
-;;                                Dumb Jump
-;; ^^^^^^^^----------------------------------------------------------------------"
-;;   ("j" dumb-jump-go "Go")
-;;   ("o" dumb-jump-go-other-window "Other window")
-;;   ("e" dumb-jump-go-prefer-external "Go external")
-;;   ("b" dumb-jump-back "Back")
-;;   ("i" dumb-jump-go-prompt "Prompt")
-;;   ("l" dumb-jump-quick-look "Quick look")
-;;   ("x" dumb-jump-go-prefer-external-other-window "Go external other window"))
-;; (global-set-key (kbd "M-g") 'hydra-dumb-jump/body)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                  Flycheck                                 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; This sets $MANPATH, $PATH and exec-path from your shell
-;; in order for flycheck to take effect
-;; (when (memq window-system '(mac ns x))
-;;   (exec-path-from-shell-initialize))
-;; Enable flycheck
-;; (add-hook 'after-init-hook 'global-flycheck-mode)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                Python mode                                ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (add-hook 'ein:notebook-multilang-mode-hook (lambda () (company-mode -1))) ;disable company mode in ein mode
-;; (setq ein:completion-backend 'ein:use-ac-jedi-backend) ;enable jedi in ein
-
-;; Use flake8 for flycheck in pyton mode
-;; (add-hook 'python-mode-hook (lambda ()
-;;                               (setq-local flycheck-python-flake8-executable "/usr/local/bin/flake8")))
-
-;; enable company-jedi
-;; (add-hook 'python-mode-hook (lambda ()
-;; 			      (make-local-variable 'company-backends)
-;; 			      (setq company-backends (copy-tree company-backends))
-;; 			      (setf (car company-backends)
-;; 			      	    (append '(company-jedi) (car company-backends)))
-;; 			      ))
-;; (require 'jedi-core)
-;; (setq python-environment-virtualenv
-;;       (append python-environment-virtualenv
-;;               '("--python" "/usr/local/bin/python3")))
-;; (add-hook 'python-mode-hook 'jedi:setup)
-
-;; Aggressive indent doesn't work for python mode, therefore
-;; electric indent mode for python mode will be automatically enabled.
-
-;; Set yas-indent-line to fixed for python mode
-;; (add-hook 'python-mode-hook (lambda ()
-;; 			      (setq-local yas-indent-line 'fixed)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                               C and C++ mode                              ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Set up c-mode and cpp-mode company backends
-;; (defun my/irony-mode ()
-;;   "Add company-irony to company-backends in C and C++ mode."
-;;   (make-local-variable 'company-backends)
-;;   (push 'company-irony company-backends)
-;;   )
-;; (add-hook 'c-mode-hook 'my/irony-mode)
-;; (add-hook 'c++-mode-hook 'my/irony-mode)
-;; (add-hook 'c-mode-hook 'irony-mode)
-;; (add-hook 'c++-mode-hook 'irony-mode)
-;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                 Java mode                                 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (defun my/lsp-java-setup ()
-;;   "Lsp java setup."
-;;   (require 'lsp-java)
-;;   (require 'lsp-ui-flycheck)
-;;   (require 'lsp-ui-sideline)
-;;   (require 'lsp-mode)
-;;   (lsp-java-enable)
-;;   (lsp-ui-sideline-mode)
-;;   (setq lsp-java--workspace-folders (list "~/javaproj"
-;; 					  "~/Android"))
-;;   (setq company-lsp-enable-snippet t
-;; 	company-lsp-cache-candidates t)
-;;   (setq lsp-ui-sideline-enable t
-;; 	lsp-ui-sideline-show-symbol t
-;; 	lsp-ui-sideline-show-hover t
-;; 	lsp-ui-sideline-show-code-actions t
-;; 	lsp-ui-sideline-update-mode 'point)
-;;   (make-local-variable 'company-backends)
-;;   (push 'company-lsp company-backends)
-;;   (lsp-ui-flycheck-enable t)
-;;   (add-hook 'lsp-mode-hook (lambda ()
-;; 			     (setq lsp-inhibit-message t
-;; 				   lsp-eldoc-render-all nil
-;; 				   lsp-highlight-symbol-at-point nil)
-;; 			     ))
-;;   )
-;; (add-hook 'java-mode-hook  'my/lsp-java-setup)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                Latex mode                                 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-;; (add-hook 'LaTeX-mode-hook 'turn-off-smartparens-mode) ; turn off smartparens since cdlatex mode has this feature
-;; (add-hook 'LaTeX-mode-hook 'cdlatex-mode)
-
-;; ;; Turn off company mode in latex-mode
-;; (add-hook 'LaTeX-mode-hook (lambda ()
-;; 			     (company-mode -1)
-;;                              (setq TeX-show-compilation t)
-;; 			     (setq reftex-plug-into-AUCTeX t)
-;; 			     (setq bibtex-completion-cite-prompt-for-optional-arguments nil)
-
-;;                              (setq bibtex-completion-bibliography ; set up helm-bibtex
-;; 				   '("~/RA/mybib.bib"
-;; 				     "~/RA/newadded.bib"))
-;; 			     (require 'helm-bibtex)
-;; 			     (helm-delete-action-from-source "Insert citation" helm-source-bibtex)
-;; 			     (helm-add-action-to-source "Insert Citation" 'helm-bibtex-insert-citation helm-source-bibtex 0) ; Set the default action to insert citation
-
-;; 			     (auctex-latexmk-setup)
-;; 			     (setq TeX-command-default "LatexMk") ; Set the default command to LatexMk
-;; 			     ))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                               Expand region                               ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (global-set-key (kbd "C-=") 'er/expand-region) ; set the expand region key
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                              Multiple cursors                             ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (global-set-key (kbd "C-c '") 'mc/edit-lines)
-;; (global-set-key (kbd "C-.") 'mc/mark-next-like-this)
-;; (global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
-;; (global-set-key (kbd "C-c ;") 'mc/mark-all-like-this)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                          Narrowing and Widening                           ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; If you're windened, narrow to the region, if you're narrowed, widen.
-;; Bound to C-c n
-
-;; (defun narrow-or-widen-dwim (p)
-
-;;   "If the buffer is narrowed, it widens.  Otherwise, it narrows intelligently.
-;; Intelligently means: region, org-src-block, org-subtree, or defun,
-;; whichever applies first.
-;; Narrowing to org-src-block actually calls `org-edit-src-code'.
-
-;;   With prefix P, don't widen, just narrow even if buffer is already
-;; narrowed."
-
-;;   (interactive "P")
-;;   (declare (interactive-only))
-;;   (cond ((and (buffer-narrowed-p) (not p)) (widen))
-;; 	((region-active-p)
-;; 	 (narrow-to-region (region-beginning) (region-end)))
-;; 	((derived-mode-p 'org-mode)
-;; 	 (cond ((ignore-errors (org-edit-src-code))
-;; 		(delete-other-windows))
-;; 	       ((org-at-block-p)
-;; 		(org-narrow-to-block))
-;; 	       (t (org-narrow-to-subtree))))
-;; 	(t (narrow-to-defun))))
-;; ;; (define-key global-map "C-c n" #'narrow-or-widen-dwim)
-;; (global-set-key (kbd "C-c n") 'narrow-or-widen-dwim)
-
+  )
 
 (provide 'init)
 
