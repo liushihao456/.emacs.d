4d8ab93504aa003dcfddff4d3a3956d90709e062
liushihao <liushihao@pku.edu.cn>
2021-02-24 14:14:58 +0800

    Move terminal-zenburn-theme to packages/

---
+++ packages/terminal-zenburn-theme/terminal-zenburn-theme-autoloads.el
@@ -0,0 +1,33 @@
+;;; terminal-zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "terminal-zenburn-theme" "terminal-zenburn-theme.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from terminal-zenburn-theme.el
+
+(defvar terminal-zenburn-override-colors-alist 'nil "\
+Place to override default theme colors.
+
+You can override a subset of the theme's default colors by
+defining them in this alist.")
+
+(custom-autoload 'terminal-zenburn-override-colors-alist "terminal-zenburn-theme" t)
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "terminal-zenburn-theme" '("terminal-zenburn")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; terminal-zenburn-theme-autoloads.el ends here
