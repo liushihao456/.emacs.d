4d45c19cbe4ac2ca4e9b4617f049bc5317e9ba53
liushihao456 <liushihao@pku.edu.cn>
2022-08-10 13:46:16 +0800

    Ignore *-autoloads.el files

--- packages/treemacs-nerd/treemacs-nerd-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; treemacs-nerd-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "treemacs-nerd" "treemacs-nerd.el" (0 0 0 0))
-;;; Generated autoloads from treemacs-nerd.el
-
-(autoload 'treemacs-nerd-config "treemacs-nerd" "\
-Install treemacs-nerd theme configuration." nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8-emacs-unix
-;; End:
-;;; treemacs-nerd-autoloads.el ends here
