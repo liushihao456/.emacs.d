3dd5c22707002b00f0b3ea3514c0545a5070c2a3
liushihao456 <liushihao@pku.edu.cn>
2022-08-15 14:29:15 +0800

    Automatically wrap isearch

--- config/init-meow.el
+++ config/init-meow.el
@@ -168,6 +168,13 @@
 				(if isearch-forward end isearch-other-end))
 	  (meow--select (not isearch-forward)))))))
 (add-hook 'isearch-mode-end-hook 'meow--post-isearch-function)
+(defadvice isearch-search (after isearch-no-fail activate)
+  (unless isearch-success
+    (ad-disable-advice 'isearch-search 'after 'isearch-no-fail)
+    (ad-activate 'isearch-search)
+    (isearch-repeat (if isearch-forward 'forward))
+    (ad-enable-advice 'isearch-search 'after 'isearch-no-fail)
+    (ad-activate 'isearch-search)))
 
 (provide 'init-meow)
 
