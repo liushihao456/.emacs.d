81111617364341cebeabfd6c3c2d74a541fc53a1
liushihao456 <liushihao@pku.edu.cn>
2022-09-02 10:34:54 +0800

    Defined variable `theme-to-load' that can be set in .dir-locals.el

--- config/init-theme.el
+++ config/init-theme.el
@@ -24,101 +24,57 @@
 ;; --------------------------------------
 
 ;;; Code:
-(require 'solar)
-(with-eval-after-load 'solar
-  (setq calendar-latitude 32.0105)
-  (setq calendar-longitude 112.0856))
 
-(defun hour-fraction-to-time (time)
-  "Convert decimal fraction TIME like 18.555 to list of (hours minutes)."
-  (let* ((time (round (* 60 time)))
-         (24-hours (/ time 60))
-         (minutes (% time 60)))
-    (list 24-hours minutes)))
+(with-eval-after-load 'base16-theme
+  (setq base16-theme-256-color-source 'base16-shell))
 
-(defun get-sunset-time ()
-  "Return the sunset time as list (hours minutes)."
-  (let ((l (solar-sunrise-sunset (calendar-current-date))))
-    (hour-fraction-to-time (caadr l))))
-
-(defun get-current-time ()
-  "Return the current time as list (hours minutes)."
-  (let ((str (current-time-string)))
-    (list
-     (string-to-number (substring str -13 -11))
-     (string-to-number (substring str -10 -8)))))
-
-(defun time-before (time1 time2)
-  "Compare TIME1 and TIME2, both in the form of list (hours minutes)."
-  (if (= (car time1) (car time2))
-      (< (cadr time1) (cadr time2))
-    (< (car time1) (car time2))))
-
-(defun load-solarized-dark ()
-  "Load solarized-dark theme."
+(with-eval-after-load 'solarized
   ;; Don't change the font for some headings and titles
   (setq solarized-use-variable-pitch nil)
   ;; Use less bolding
-  ;; (setq solarized-use-less-bold t)
+  (setq solarized-use-less-bold t)
   ;; Use more italics
-  ;; (setq solarized-use-more-italic t)
+  (setq solarized-use-more-italic t)
   ;; Use less colors for indicators such as git:gutter, flycheck and similar
   (setq solarized-emphasize-indicators nil)
   ;; Don't change size of org-mode headlines (but keep other size-changes)
-  (setq solarized-scale-org-headlines nil)
-  (load-theme 'solarized-dark t))
+  (setq solarized-scale-org-headlines nil))
 
-(defun load-zenburn ()
-  "Load zenburn theme."
-  (load-theme 'zenburn t)
-  (custom-set-faces
-   `(lsp-ui-doc-background ((t (:background "#272A36"))))))
-
-(defun load-spacemacs-theme ()
-  "Load spacemacs theme."
-  (load-theme 'spacemacs-dark t))
-
-;; (let ((current-time (get-current-time))
-;;       (sunset-time (get-sunset-time)))
-;;   (message "Sunset time: %s: %s" (car sunset-time) (cadr sunset-time))
-;;   (setcar sunset-time (- (car sunset-time) 1))
-;;   (if (time-before current-time sunset-time)
-;;       (load-zenburn)
-;;     (load-solarized-dark)))
-
-;; (load-solarized-dark)
-;; (load-zenburn)
-;; (load-spacemacs-theme)
+;; This variable can be set in .dir-locals.el
+(defvar theme-to-load nil
+  "Theme to use.")
+(put 'theme-to-load 'safe-local-variable #'custom-theme-p)
 
 (defun load-base16-theme (&optional theme)
   "Load base16 theme.
 
-If called with the optional arg THEME, then THEME is loaded, otherwise the theme
-in ``~/.vimrc_background'' which base16-shell produces is loaded.
+If called with the optional arg THEME, then THEME is loaded.
+THEME could be a string or a symbol.
 
-THEME could be a string or a symbol."
-  (if theme
+Otherwise tries to load the theme in ``~/.vimrc_background''
+which base16-shell produces."
+  (cond
+   (theme
       (cond ((symbolp theme)
              (load-theme theme t))
             ((stringp theme)
-             (load-theme (intern theme) t)))
-    (if (file-exists-p "~/.vimrc_background")
+             (load-theme (intern theme) t))))
+   ((file-exists-p "~/.vimrc_background")
         (with-temp-buffer
           (insert-file-contents "~/.vimrc_background")
           (save-match-data
-            (let ((theme1 (buffer-substring-no-properties (search-forward "colorscheme ") (point-at-eol))))
+            (let ((theme1 (buffer-substring-no-properties
+                           (search-forward "colorscheme ") (point-at-eol))))
               (message "Loaded %s theme" theme1)
-              (load-theme (intern theme1) t))))
-      (message "No theme is specified and no .vimrc_background file found."))))
+              (load-theme (intern theme1) t)))))
+   (t
+    (message "No theme is specified and no .vimrc_background file found."))))
 
-;; (load-theme 'doom-one t)
-(with-eval-after-load 'base16-theme
-  (setq base16-theme-256-color-source 'base16-shell))
-(if (display-graphic-p)
-    (load-base16-theme 'base16-tomorrow-night-eighties)
+(if theme-to-load
+    (load-theme theme-to-load t)
   (load-base16-theme))
-(setq frame-background-mode 'dark)
 
+(setq frame-background-mode 'dark)
 (unless (display-graphic-p)
   (set-face-background 'default 'unspecified))
 
