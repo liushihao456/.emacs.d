abdae4f01101708631ad9aa7f1a6ec6fde8f70f4
liushihao456 <liushihao@pku.edu.cn>
2022-09-07 17:01:02 +0800

    Removed yasnippet-snippets; added custom snippets

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -26,6 +26,12 @@
 ;;; Code:
 
 (with-eval-after-load 'yasnippet
+  (defun yas-try-key-from-dot (_start-point)
+    "As `yas-key-syntaxes' element, look for dot key.
+It enables expanding `foo.' to `foo->'."
+    (skip-chars-backward "\."))
+  (add-to-list 'yas-key-syntaxes 'yas-try-key-from-dot)
+
   (setq yas-triggers-in-field t)
   (yas-reload-all))
 (add-hook 'prog-mode-hook 'yas-minor-mode)
