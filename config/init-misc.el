9d13d25122a89b1e113bb97e084779fdad8b69ad
liushihao456 <liushihao@pku.edu.cn>
2022-09-24 13:51:59 +0800

    Deleted helpful

--- config/init-misc.el
+++ config/init-misc.el
@@ -242,14 +242,6 @@ split; vice versa."
 (global-set-key (kbd "M-`") 'save-buffer)
 (global-set-key (kbd "M-e") 'forward-paragraph)
 (global-set-key (kbd "M-a") 'backward-paragraph)
-;; Helpful
-;;
-;; Note that the built-in `describe-function' includes both functions
-;; and macros. `helpful-function' is functions only, so we provide
-;; `helpful-callable' as a drop-in replacement.
-(global-set-key (kbd "C-h f") 'helpful-callable)
-(global-set-key (kbd "C-h v") 'helpful-variable)
-(global-set-key (kbd "C-h k") 'helpful-key)
 ;; Magit
 (global-set-key (kbd "C-c g") 'magit-status)
 ;; Expand region
