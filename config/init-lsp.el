f73d4f596ace6d92041a98bc8b5ec78356d3fd52
liushihao456 <liushihao@pku.edu.cn>
2022-10-10 22:23:51 +0800

    Added eldoc-box that displays eldoc in floating frame

--- config/init-lsp.el
+++ config/init-lsp.el
@@ -85,54 +85,7 @@ typing or company is active."
 ;; Show eldoc in posframe ---------------------------------------------------- ;
 
 (when (display-graphic-p)
-  (defvar eldoc-posframe-buffer "*eldoc-posframe-buffer*"
-    "The posframe buffer name use by eldoc-posframe.")
-
-  (defvar eldoc-posframe-hide-posframe-hooks
-    '(pre-command-hook post-command-hook focus-out-hook)
-    "The hooks which should trigger automatic removal of the posframe.")
-
-  (defvar eldoc-posframe-delay 0.2
-    "Delay seconds to display `eldoc'.")
-
-  (defvar-local eldoc-posframe--timer nil)
-
-  (defun eldoc-posframe-hide-posframe ()
-    "Hide messages currently being shown if any."
-    (when eldoc-posframe--timer
-      (cancel-timer eldoc-posframe--timer))
-
-    (posframe-hide eldoc-posframe-buffer)
-    (dolist (hook eldoc-posframe-hide-posframe-hooks)
-      (remove-hook hook #'eldoc-posframe-hide-posframe t)))
-
-  (defun eldoc-posframe-show-posframe (str &rest args)
-    "Display STR with ARGS."
-    (require 'posframe)
-    (when eldoc-posframe--timer
-      (cancel-timer eldoc-posframe--timer))
-
-    (posframe-hide eldoc-posframe-buffer)
-    (dolist (hook eldoc-posframe-hide-posframe-hooks)
-      (add-hook hook #'eldoc-posframe-hide-posframe nil t))
-
-    (setq eldoc-posframe--timer
-          (run-with-idle-timer
-           eldoc-posframe-delay nil
-           (lambda ()
-             (when str
-               (posframe-show
-                eldoc-posframe-buffer
-                :string (apply #'format str args)
-                :postion (point)
-                :poshandler #'posframe-poshandler-point-bottom-left-corner-upward
-                :left-fringe 8
-                :right-fringe 8
-                :internal-border-width 1
-                :internal-border-color (face-attribute 'font-lock-comment-face :foreground)
-                :background-color (face-background 'tooltip)))))))
-
-  (setq eldoc-message-function #'eldoc-posframe-show-posframe))
+  (add-hook 'prog-mode-hook 'eldoc-box-hover-mode))
 
 (provide 'init-lsp)
 
