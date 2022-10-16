dc1e1156f218d8d08df11ea9ae86a6009f5e8750
liushihao456 <liushihao@pku.edu.cn>
2022-10-17 01:02:09 +0800

    Added some diff-hl configs

--- config/init-misc.el
+++ config/init-misc.el
@@ -375,36 +375,39 @@ This function is useful as a `:filter' to a conditional key definition."
   '(menu-item "" my/tab-jump-over-pair :filter my/tab-jump-over-pair-key-filter))
 
 ;; Diff-hl mode
+(setq diff-hl-command-prefix (kbd "C-c v"))
+(with-eval-after-load 'diff-hl
+  (defun my/diff-hl-define-bitmaps (&rest _)
+    (define-fringe-bitmap 'diff-hl-bmp-middle [#b00011000] nil nil '(center repeated))
+    (define-fringe-bitmap 'diff-hl-bmp-delete [#b11110000
+                                               #b11100000
+                                               #b11000000
+                                               #b10000000] nil nil 'top))
+
+  (advice-add #'diff-hl-define-bitmaps :override #'my/diff-hl-define-bitmaps)
+  (defun my/diff-hl-type-face-fn (type _pos)
+    (intern (format "diff-hl-%s" type)))
+  (defun my/diff-hl-type-at-pos-fn (type _pos)
+    (if (eq type 'delete)
+        'diff-hl-bmp-delete
+      'diff-hl-bmp-middle))
+  (advice-add #'diff-hl-fringe-bmp-from-pos  :override #'my/diff-hl-type-at-pos-fn)
+  (advice-add #'diff-hl-fringe-bmp-from-type :override #'my/diff-hl-type-at-pos-fn)
+  (setq diff-hl-draw-borders nil)
+  (with-eval-after-load 'flycheck
+    (setq flycheck-indication-mode 'right-fringe)
+    ;; Let the arrow point left
+    (when (fboundp 'define-fringe-bitmap) ;; #ifdef HAVE_WINDOW_SYSTEM
+      (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
+        flycheck-fringe-bitmap-double-left-arrow)
+      (define-fringe-bitmap
+        'flycheck-fringe-bitmap-double-arrow-hi-res
+        flycheck-fringe-bitmap-double-left-arrow-hi-res
+        nil 16))))
+
 (global-diff-hl-mode)
 (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
 (add-hook 'vc-dir-mode-hook 'diff-hl-dir-mode)
-(defun my/diff-hl-define-bitmaps (&rest _)
-  (define-fringe-bitmap 'diff-hl-bmp-middle [#b00011000] nil nil '(center repeated))
-  (define-fringe-bitmap 'diff-hl-bmp-delete [#b11110000
-                                             #b11100000
-                                             #b11000000
-                                             #b10000000] nil nil 'top))
-
-(advice-add #'diff-hl-define-bitmaps :override #'my/diff-hl-define-bitmaps)
-(defun my/diff-hl-type-face-fn (type _pos)
-  (intern (format "diff-hl-%s" type)))
-(defun my/diff-hl-type-at-pos-fn (type _pos)
-  (if (eq type 'delete)
-      'diff-hl-bmp-delete
-    'diff-hl-bmp-middle))
-(advice-add #'diff-hl-fringe-bmp-from-pos  :override #'my/diff-hl-type-at-pos-fn)
-(advice-add #'diff-hl-fringe-bmp-from-type :override #'my/diff-hl-type-at-pos-fn)
-(setq diff-hl-draw-borders nil)
-(with-eval-after-load 'flycheck
-  (setq flycheck-indication-mode 'right-fringe)
-  ;; Let the arrow point left
-  (when (fboundp 'define-fringe-bitmap) ;; #ifdef HAVE_WINDOW_SYSTEM
-    (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
-      flycheck-fringe-bitmap-double-left-arrow)
-    (define-fringe-bitmap
-      'flycheck-fringe-bitmap-double-arrow-hi-res
-      flycheck-fringe-bitmap-double-left-arrow-hi-res
-      nil 16)))
 
 ;; Rainbow delimiters
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
