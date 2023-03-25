6a0de5628f549e14e2023b6d3c4c891b354036cb
liushihao456 <liushihao@pku.edu.cn>
2023-03-25 16:07:40 +0800

    Updated my/cmake-project-generate-compile-commands

--- config/init-cpp.el
+++ config/init-cpp.el
@@ -19,7 +19,7 @@ project in order for clangd to understand the project code."
     (let ((default-directory (project-root (project-current))))
       (shell-command
        (if (memq system-type '(ms-dos windows-nt cygwin))
-           "cmake . -G Ninja -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=YES"
+           "cmake . -G Ninja -Bbuild_nj -DCMAKE_EXPORT_COMPILE_COMMANDS=YES && move \".\\build_nj\\compile_commands.json\" \".\""
          "cmake . -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=YES"))))
   (define-key c-mode-base-map (kbd "C-c l s") 'my/cmake-project-generate-compile-commands)
 
